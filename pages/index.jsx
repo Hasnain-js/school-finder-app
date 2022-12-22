fd9664cfaf77c30431e6de157975c54c44bde788
Ramsha Naseer <chahatkhan534@gmail.com>
2022-12-22 07:18:34 -1200

    initialize react project

---
+++ pages/index.jsx
@@ -0,0 +1,22 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from '@next/font/google'
+import styles from '../styles/Home.module.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>School Finder App</title>
+        <meta name="description" content="You can find your dream school" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={styles.main}>
+       <h1>Welcome to school finder Application</h1>
+      </main>
+    </>
+  )
+}
