ef94571f496fb77c7eb3f86ee04df8336c0040f7
Hasnain-js <ihasnain4@gmail.com>
2023-03-25 15:55:29 +0500

    set school card responsiveness

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import Image from "next/image";
 import { Autoplay, Navigation, Pagination } from "swiper";
 import { SchoolCard } from "@/components/SchoolCard";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
@@ -17,45 +18,91 @@ export default function Home() {
       <main>
         {/* Heroic Section */}
         <div className='relative bg-almond overflow-hidden bg-no-repeat bg-contain bg-center bg-[url("/images/Heroic-Banner.png")]'>
-					<div className='mx-auto max-w-7xl px-6'>
-						<div className='grid items-center grid-cols-1 gap-12 lg:grid-cols-2 justify-items-center'>
-							<div className='space-y-4'>
-								<h1 className='font-bold text-black lg:text-7xl text-6xl'>
+          <div className="mx-auto max-w-7xl px-6">
+            <div className="grid items-center grid-cols-1 gap-12 lg:grid-cols-2 justify-items-center">
+              <div className="flex flex-col items-start gap-9">
+                <h1 className="font-bold text-black lg:text-7xl text-6xl">
                   Get
                   <span className="text-rustyRed"> Schools</span>
                   <div className="relative inline-flex">
                     <span className="absolute inset-x-0 bottom-0"></span>
-                    <h1 className=" flex relative font-bold text-black text-6xl lg:text-7xl">
+                    <div className=" flex relative font-bold text-black text-6xl lg:text-7xl">
                       Inside
-                      <span className="w-fit">
+                      {/* -top-2 mt-3 relative */}
+                      <span className="w-fit relative -mt-1">
+                        <h2 className="text-rustyRed -rotate-[10deg] absolute top-1 left-4">
+                          Scoop!
+                        </h2>
                         <svg
-                          className="relative -top-2 ml-3"
-                          height="100"
+                          className="absolute -top-4"
+                          width="260"
+                          height="130"
                           viewBox="0 0 361 172"
                           fill="none"
                           xmlns="http://www.w3.org/2000/svg"
                         >
                           <path
-                            d="M72.7823 70.0114C66.771 66.0393 59.6454 65.1322 52.5155 66.1342C40.1272 67.8753 29.4415 76.557 31.3328 90.0147C34.2889 111.048 63.0013 100.651 64.6296 112.237C65.2559 116.693 60.4764 119.546 55.9311 120.185C51.2966 120.836 46.349 119.259 42.971 115.917L34.6063 128.181C41.3542 132.867 48.8224 134.271 56.9327 133.132C69.9448 131.303 80.3241 123.028 78.3451 108.946C75.2387 86.8433 47.2239 98.324 45.6081 86.8269C44.9442 82.1034 50.1303 79.5568 54.0518 79.0057C57.4385 78.5297 62.0826 79.2403 64.7115 81.7791L72.7823 70.0114ZM122.424 82.9384C117.634 79.2491 111.272 78.5983 106.192 79.3122C92.8229 81.1911 83.3806 91.6065 85.322 105.421C87.2635 119.235 99.211 126.644 112.58 124.766C117.66 124.052 123.596 121.672 127.184 116.806L117.058 108.777C115.793 110.772 113.867 112.588 110.926 113.001C104.242 113.941 99.605 110.048 98.6907 103.542C97.7763 97.0359 101.161 92.016 107.845 91.0766C110.608 90.6883 113.304 91.7636 114.904 93.4473L122.424 82.9384ZM128.05 99.4157C129.992 113.23 141.939 120.639 155.308 118.76C168.676 116.882 178.119 106.466 176.177 92.6519C174.236 78.8377 162.288 71.4284 148.92 73.3072C135.551 75.186 126.109 85.6015 128.05 99.4157ZM141.419 97.5369C140.504 91.0309 143.889 86.011 150.573 85.0716C157.257 84.1322 161.894 88.0247 162.809 94.5308C163.723 101.037 160.339 106.057 153.654 106.996C146.97 107.936 142.333 104.043 141.419 97.5369ZM182.447 91.7707C184.389 105.585 196.336 112.994 209.705 111.115C223.073 109.237 232.516 98.8212 230.574 85.0069C228.633 71.1927 216.685 63.7833 203.317 65.6622C189.948 67.541 180.506 77.9565 182.447 91.7707ZM195.816 89.8919C194.901 83.3858 198.286 78.366 204.97 77.4266C211.654 76.4871 216.291 80.3797 217.206 86.8858C218.12 93.3918 214.736 98.4117 208.051 99.3511C201.367 100.291 196.73 96.398 195.816 89.8919ZM245.596 125.702L258.964 123.824L255.457 98.8688L255.635 98.8437C259.254 102.607 264.636 103.395 270.162 102.619C282.461 100.89 288.395 88.7867 286.704 76.7549C284.862 63.6537 275.093 55.5747 262.259 57.3784C256.199 58.2301 251.543 62.6107 249.931 66.0181L249.753 66.0432L248.952 60.3392L236.652 62.0678L245.596 125.702ZM251.995 81.9964C251.081 75.4903 254.465 70.4705 261.15 69.531C267.834 68.5916 272.471 72.4842 273.385 78.9902C274.299 85.4963 270.915 90.5162 264.231 91.4556C257.547 92.395 252.91 88.5024 251.995 81.9964ZM310.508 31.5117L297.139 33.3905L303.077 75.6353L316.445 73.7565L310.508 31.5117ZM303.415 89.0386C304.054 93.584 308.222 96.7245 312.767 96.0857C317.312 95.4469 320.453 91.2793 319.814 86.7339C319.175 82.1886 315.008 79.0481 310.462 79.6869C305.917 80.3257 302.776 84.4933 303.415 89.0386Z"
-                            fill="#F05F2D"
-                          />
-                          <path
-                            fillRule="evenodd"
-                            clipRule="evenodd"
-                            d="M343.351 7.26863C343.43 9.52688 342.251 11.5372 340.442 12.6243L354.424 112.112C357.08 112.653 359.119 114.955 359.218 117.782C359.334 121.094 356.743 123.872 353.431 123.988C351.352 124.061 349.484 123.067 348.352 121.498L29.3683 166.328C28.6252 168.683 26.4634 170.427 23.8542 170.518C20.5425 170.634 17.7641 168.043 17.6485 164.731C17.5635 162.298 18.9394 160.153 20.9888 159.14L7.05062 59.965C4.16711 59.6164 1.88647 57.2116 1.78192 54.2178C1.66628 50.9061 4.25718 48.1277 7.56887 48.0121C10.0437 47.9257 12.2207 49.3507 13.2112 51.4585L331.4 6.74003C331.753 3.86141 334.156 1.5861 337.146 1.48168C340.457 1.36604 343.236 3.95694 343.351 7.26863ZM13.7653 54.4101L331.786 9.71523C332.698 11.9839 334.95 13.552 337.532 13.4754L351.417 112.268C348.908 113.058 347.129 115.445 347.225 118.201C347.23 118.343 347.24 118.483 347.255 118.622L29.5154 163.278C28.9587 160.636 26.673 158.657 23.9323 158.529L10.0251 59.5736C12.1174 58.7287 13.6116 56.7422 13.7653 54.4101Z"
+                            fill-rule="evenodd"
+                            clip-rule="evenodd"
+                            d="M343.352 7.26863C343.43 9.52688 342.251 11.5372 340.442 12.6243L354.424 112.112C357.08 112.653 359.119 114.955 359.218 117.782C359.334 121.094 356.743 123.872 353.431 123.988C351.352 124.061 349.484 123.067 348.352 121.498L29.3684 166.328C28.6252 168.683 26.4634 170.427 23.8542 170.518C20.5425 170.634 17.7641 168.043 17.6485 164.731C17.5635 162.298 18.9395 160.153 20.9888 159.14L7.05065 59.965C4.16714 59.6164 1.8865 57.2116 1.78195 54.2178C1.66631 50.9061 4.25721 48.1277 7.5689 48.0121C10.0437 47.9257 12.2207 49.3507 13.2112 51.4585L331.4 6.74003C331.753 3.86141 334.156 1.5861 337.146 1.48168C340.457 1.36604 343.236 3.95694 343.352 7.26863ZM13.7653 54.4101L331.786 9.71523C332.698 11.9839 334.95 13.552 337.532 13.4754L351.417 112.268C348.908 113.058 347.129 115.445 347.225 118.201C347.23 118.343 347.24 118.483 347.255 118.622L29.5154 163.278C28.9587 160.636 26.673 158.657 23.9323 158.529L10.0251 59.5736C12.1175 58.7287 13.6117 56.7422 13.7653 54.4101Z"
                             fill="#D15125"
                           />
+                          <g filter="url(#filter0_d_214_87)">
+                            <path
+                              d="M185.526 133.125C185.475 132.301 186.388 131.773 187.077 132.23L209.403 147.033C210.21 147.568 209.862 148.823 208.895 148.866L196.518 149.418C196.193 149.432 195.896 149.604 195.72 149.878L189.024 160.345C188.503 161.159 187.243 160.833 187.183 159.868L185.526 133.125Z"
+                              fill="#2B3240"
+                            />
+                          </g>
+                          <defs>
+                            <filter
+                              id="filter0_d_214_87"
+                              x="182.524"
+                              y="131.061"
+                              width="30.3284"
+                              height="34.7461"
+                              filterUnits="userSpaceOnUse"
+                              color-interpolation-filters="sRGB"
+                            >
+                              <feFlood
+                                flood-opacity="0"
+                                result="BackgroundImageFix"
+                              />
+                              <feColorMatrix
+                                in="SourceAlpha"
+                                type="matrix"
+                                values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+                                result="hardAlpha"
+                              />
+                              <feOffset dy="2" />
+                              <feGaussianBlur stdDeviation="1.5" />
+                              <feComposite in2="hardAlpha" operator="out" />
+                              <feColorMatrix
+                                type="matrix"
+                                values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.45 0"
+                              />
+                              <feBlend
+                                mode="normal"
+                                in2="BackgroundImageFix"
+                                result="effect1_dropShadow_214_87"
+                              />
+                              <feBlend
+                                mode="normal"
+                                in="SourceGraphic"
+                                in2="effect1_dropShadow_214_87"
+                                result="shape"
+                              />
+                            </filter>
+                          </defs>
                         </svg>
                       </span>
-                    </h1>
+                    </div>
                   </div>
                 </h1>
-                <p className="mt-8 text-base text-black sm:text-xl">
-                  Find the perfect school for your child with just a few taps
-                  using our intuitive school finder app.
-                </p>
-
-                <form>
+                <section className="space-y-4">
+                  <p className="mt-8 text-base text-black sm:text-xl">
+                    Find the perfect school for your child with just a few taps
+                    using our intuitive school finder app.
+                  </p>
                   <div className="relative max-w-md flex items-center justify-between shadow-2xl shadow-[#B1B1B1] w-full p-1 text-sm text-gray-900 rounded-[40px] bg-gray-50">
                     <div className="flex w-full items-center gap-3">
                       <span className="w-fit h-fit ml-5">
@@ -89,7 +136,7 @@ export default function Home() {
                       Search
                     </button>
                   </div>
-                </form>
+                </section>
               </div>
               <Image
                 width={776}
@@ -307,41 +354,49 @@ export default function Home() {
         {/* lower banner section end */}
 
         {/* Our schools */}
-        <section className="py-24 container mx-auto">
+        <section className="py-24 px-6 max-w-7xl mx-auto">
           <div className=" flex-col">
             <h2 className="text-black text-center text-3xl font-semibold leading-tight sm:text-4xl lg:text-5xl lg:leading-tight">
               Our Schools
             </h2>
-            <div className="relative max-w-7xl mx-auto">
-              <Swiper
-                slidesPerView={3}
-                spaceBetween={50}
-                centeredSlides={true}
-                navigation={true}
-                autoplay={{
-                  delay: 2500,
-                  disableOnInteraction: false,
-                }}
-                pagination={{
-                  clickable: true,
-                }}
-                modules={[Navigation, Autoplay]}
-                className="mySwiper"
-              >
-                {[1, 2, 3, 4, 5, 6].map((items) => {
-                  return (
-                    <SwiperSlide key={items}>
-                      <SchoolCard
-                        category={schoolCardData.category}
-                        title={schoolCardData.title}
-                        image={schoolCardData.image}
-                        ratings={schoolCardData.ratings}
-                      />
-                    </SwiperSlide>
-                  );
-                })}
-              </Swiper>
-            </div>
+            <Swiper
+              centeredSlides={true}
+              slidesPerView={1}
+              spaceBetween={10}
+              navigation={true}
+              breakpoints={{
+                640: {
+                  slidesPerView: 2,
+                  spaceBetween: 20,
+                },
+                768: {
+                  slidesPerView: 2,
+                  spaceBetween: 40,
+                },
+                1024: {
+                  slidesPerView: 3,
+                  spaceBetween: 50,
+                },
+              }}
+              pagination={{
+                dynamicBullets: true,
+              }}
+              modules={[Navigation, Autoplay, Pagination]}
+              className="custom-swiper"
+            >
+              {[1, 2, 3, 4, 5, 6].map((items) => {
+                return (
+                  <SwiperSlide key={items}>
+                    <SchoolCard
+                      category={schoolCardData.category}
+                      title={schoolCardData.title}
+                      image={schoolCardData.image}
+                      ratings={schoolCardData.ratings}
+                    />
+                  </SwiperSlide>
+                );
+              })}
+            </Swiper>
           </div>
         </section>
 
